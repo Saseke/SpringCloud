a839e32d74bd9d71314120f08e5c8b16ed0fe1b6
Yoke-Yoke <717964921@qq.com>
2018-01-18 08:26:04 +0800

    add hystrix,turbine of use

---
+++ microservice-spring-cloud/microservice-consumer-movie-ribbon-with-hystrix2/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/controller/MoviceController.java
@@ -0,0 +1,34 @@
+package com.itmuch.cloud.microservicesimpleconsumermovie.controller;
+
+import com.itmuch.cloud.microservicesimpleconsumermovie.entity.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * hystrix的配置
+ */
+@RestController
+public class MoviceController {
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public MoviceController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("/movie/{id}")
+    @HystrixCommand(fallbackMethod = "findByIdFallback")
+    public User findById(@PathVariable Long id) {
+        return this.restTemplate.getForObject("http://microservice-provider-user/simple/" + id, User.class);
+    }
+
+    public User findByIdFallback(Long id) {
+        User user = new User();
+        user.setId(0L);
+        return user;
+    }
+}
